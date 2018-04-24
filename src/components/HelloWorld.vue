de5f14b6eb3543ac0e7b209e01d5bc3588696436
Luca Pilloni <pilloni.luca@hotmail.fr>
2018-04-24 14:33:29 +0200

    adding the info file and makeing few changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App: This is sparta !'
+      msg: 'This is the Git Amend Demo!'
     }
   }
 }
